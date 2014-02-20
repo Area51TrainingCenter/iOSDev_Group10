c0f4b89bab81188764b2539d13067c0437eeda00
eldervaz <eldervaz@gmail.com>
2014-02-20 10:33:21 -0500

    first class

---
+++ module_1/class_1/MiPrimeraAppGrupo10/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MiPrimeraAppGrupo10
+//
+//  Created by IOS on 19/02/14.
+//  Copyright (c) 2014 Area 51. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
